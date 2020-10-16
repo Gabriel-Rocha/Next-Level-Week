2268c5ffbbfb96c26cb460ee2edf63d1acdad717
Gabriel-Rocha <53313433+Gabriel-Rocha@users.noreply.github.com>
2020-10-16 20:55:01 -0300

    d

--- www/nlw/backend/src/server.ts
+++ www/nlw/backend/src/server.ts
@@ -3,8 +3,21 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+//Rota = Conjunto
+//recurso = Usuarios
+
+// Metodos HTTP = GET, POST, PUT, DELETE
+// Parametros 
+
+// GET = Buscar uma informação (Lista, item)
+// POST = Criando uma informação 
+// PUT = Editar uma informação
+// DELETE = Delete uma informação
+
 app.get('/users', (request, response) => {
-    console.log('teste');
+    return response.json({ message: 'teste'});
 });
 
 app.listen(3333);
